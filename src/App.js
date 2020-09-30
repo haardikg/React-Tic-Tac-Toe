6448d85bde825b1db555d6acfab4004ea0994f57
Haardik Gupta <haardik21@icloud.com>
2020-09-30 13:12:49 -0400

    push

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Board from './components/Board';
+
+const PLAYER_1 = 'X';
+const PLAYER_2 = 'O';
+
+const generateSquares = () => {
+  const squares = [];
+
+  let currentId = 0;
+
+  for (let row = 0; row < 3; row += 1) {
+    squares.push([]);
+    for (let col = 0; col < 3; col += 1) {
+      squares[row].push({
+        id: currentId,
+        value: '',
+      });
+      currentId += 1;
+    }
+  }
+
+  return squares;
+}
+
+const App = () => {
+
+  const [squares, setSquares] = useState(generateSquares());
+
+  // Wave 2
+  // You will need to create a method to change the square 
+  //   When it is clicked on.
+  //   Then pass it into the squares as a callback
+
+
+  const checkForWinner = () => {
+    // Complete in Wave 3
+
+  }
+
+  const resetGame = () => {
+    // Complete in Wave 4
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Tic Tac Toe</h1>
+        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <button>Reset Game</button>
+      </header>
+      <main>
+        <Board squares={squares} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
